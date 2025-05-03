0e1196d5529a8a90b0da933d8b00db7aed0f7711
fire8327 <fire83274@gmail.com>
2025-05-04 01:15:14 +0300

    add tailwind, icon

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <p class="mainHeading">
+        Главная страница
+        <Icon class="text-3xl" name="ri:accessibility-fill"/>
+    </p>
+</template>
+
+<script setup>
+
+</script>
