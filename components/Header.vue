41e314aebdee6af768625d2709ee00b45b859965
fire8327 <fire83274@gmail.com>
2025-05-04 20:28:04 +0300

    create reg, auth

--- components/Header.vue
+++ components/Header.vue
@@ -7,7 +7,7 @@
             <div class="flex items-center gap-6">
                 <NuxtLink to="/" class="relative after:w-0 after:h-[2px] after:bg-violet-500 after:rounded-full after:absolute after:top-full after:left-1/2 after:-translate-x-1/2 after:transition-all after:duration-500 hover:after:w-full">Тест</NuxtLink>
                 <div class="flex items-center gap-2">
-                    <NuxtLink to="/" class="flex">
+                    <NuxtLink to="/auth" class="flex">
                         <Icon class="text-3xl text-violet-500" name="material-symbols:account-circle-outline"/>
                     </NuxtLink>
                 </div>
