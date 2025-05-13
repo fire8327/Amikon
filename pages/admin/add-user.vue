a9cd479fa37dc6dcd9c6bc16eab6d44e809aaf8f
fire8327 <fire83274@gmail.com>
2025-05-13 17:18:10 +0300

    upd of adding user

---
+++ pages/admin/add-user.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="flex items-center justify-between">
+        <p class="mainHeading">Добавление пользователя</p>
+        <button @click="goBack" type="button" class="px-4 py-2 w-40 border border-violet-500 hover:bg-violet-500 hover:text-white rounded-full text-center transition-all duration-500 text-violet-500 bg-transparent">Назад</button>
+    </div>
+    <FormKit v-model="userForm" @submit="addUser" type="form" :actions="false" messages-class="hidden" form-class="flex flex-col gap-6 items-center justify-center grow">
+        <div class="flex items-center lg:items-start gap-4 max-lg:flex-col w-full md:w-2/3 lg:w-1/2">
+            <FormKit v-model="user.surname" validation="required" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Фамилия" name="Фамилия" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+            <FormKit v-model="user.name" validation="required" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Имя" name="Имя" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+            <FormKit v-model="user.patronymic" validation="required" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Отчество" name="Отчество" outer-class="w-full lg:w-1/3" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+        </div>
+        <div class="flex items-center lg:items-start gap-4 max-lg:flex-col w-full md:w-2/3 lg:w-1/2">
+            <FormKit v-model="user.login" validation="required" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Логин" name="Логин" outer-class="w-full lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+            <FormKit v-model="user.password" validation="required|length:6" messages-class="text-[#E9556D] font-mono" type="password" placeholder="······" name="Пароль" outer-class="w-full lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+        </div>
+        <FormKit v-model="user.phone" validation="required" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Телефон" name="Телефон" outer-class="w-full md:w-2/3 lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+        <FormKit v-model="user.email" validation="required|email" messages-class="text-[#E9556D] font-mono" type="text" placeholder="Email" name="Email" outer-class="w-full md:w-2/3 lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+        <FormKit v-model="user.company" :options="companies" validation="required" messages-class="text-[#E9556D] font-mono" type="select" placeholder="Компания заказчик" name="Компания заказчик" outer-class="w-full md:w-2/3 lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+        <FormKit @change="(e) => { logoFile = e.target.files[0] }" accept="image/*" validation="required" messages-class="text-[#E9556D] font-mono" type="file" label="Фото профиля" name="Фото профиля" outer-class="w-full md:w-2/3 lg:w-1/2" input-class="focus:outline-none px-4 py-2 bg-white rounded-xl border border-transparent w-full transition-all duration-500 focus:border-violet-500 shadow-md"/>
+        <button :disabled="isAddDisabled" :class="{ 'opacity-50 cursor-not-allowed': isAddDisabled }" type="submit" class="px-4 py-2 border border-violet-500 bg-violet-500 text-white rounded-full w-[160px] text-center transition-all duration-500 hover:text-violet-500 hover:bg-transparent">Добавить</button>
+    </FormKit>
+</template>
+
+<script setup>
+/* подключение сообщений */
+const { showMessage } = useMessagesStore()
+
+
+/* подключение роутера и бд */
+const router = useRouter()
+const supabase = useSupabaseClient()
+
+
+/* функция возвращения назад */
+const goBack = () => {
+  if (window.history.length > 1) {
+    router.back()
+  } else {
+    router.push('/')
+  }
+}
+
+
+/* добавление нового пользователя */
+// отдельно форма для очистки
+const userForm = ref(null)
+
+// данные о пользователе
+const user = ref({
+    surname: "",
+    name: "",
+    patronymic: "",
+    login: "",
+    password: "",
+    phone: "",
+    email: "",
+    company: "",
+    image: "",
+    is_approved: true
+})
+
+//изборажение
+const logoFile = ref(null)
+
+
+/* получение компаний */
+const companies = ref([])
+const getCompanies = async() => {
+    const { data } = await supabase
+    .from('companies')
+    .select('name')
+
+    companies.value = data.map(item => item.name) || []
+}
+
+
+/* регистрация пользователя */
+const isAddDisabled = ref(false)
+const addUser = async () => {
+    isAddDisabled.value = true
+    const { data: users, error: usersError } = await supabase
+    .from('users')
+    .select("*")
+    .eq('login', user.value.login)
+
+    if (users[0]) {
+        user.value.login = ""
+        isAddDisabled.value = false
+        return showMessage("Такой  логин уже используется!", false)
+    } 
+    
+    // изображение
+    if (logoFile.value) {
+        const file = logoFile.value
+        const extension = file.name.split('.').pop()
+        const fileName = `${Date.now()}-${Math.random().toString(36).substring(2, 7)}.${extension}`
+
+        const { error: uploadError } = await supabase.storage
+        .from('files/logos')
+        .upload(`${fileName}`, file)
+
+        if (uploadError) throw uploadError
+
+        user.value.image = fileName
+    }
+
+
+    const { data, error } = await supabase
+    .from('users')
+    .insert(user.value)
+    .select()
+
+    if (data) {
+        console.log(data)
+        showMessage('Успешное добавление!', true)
+        isAddDisabled.value = false
+        userForm.value = {}
+        router.push("/admin")
+    } else {
+        console.log(user.value)            
+        isAddDisabled.value = false
+        showMessage('Произошла ошибка!', false)
+    }
+}
+
+
+/* первоначальная загрука */
+onMounted(() => {
+    getCompanies()
+})
+</script>
